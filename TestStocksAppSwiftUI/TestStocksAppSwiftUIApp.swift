f439a9419355347f63feb0914816ffc1ade9a166
Чесников <maxim.chesnikov@chulakov.ru>
2021-02-18 20:42:03 +0300

    Initial Commit

---
+++ TestStocksAppSwiftUI/TestStocksAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestStocksAppSwiftUIApp.swift
+//  TestStocksAppSwiftUI
+//
+//  Created by Max Chesnikov on 18.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestStocksAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
